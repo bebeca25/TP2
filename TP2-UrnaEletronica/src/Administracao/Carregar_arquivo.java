578845cec67865d3cccf2dcda866a1e25a3ef44c
Janu23 <victorjdacruz@gmail.com>
2015-11-13 14:37:27 -0200

    Commit escrever_arquivo.java

--- TP2-UrnaEletronica/src/Administracao/Carregar_arquivo.java
+++ TP2-UrnaEletronica/src/Administracao/Carregar_arquivo.java
@@ -3,18 +3,24 @@ package Administracao;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import java.util.Scanner;
 
 
 public class Carregar_arquivo {
-    public static void carregar_arquivo( ArrayList<Eleitor> ListaEleitor ) throws FileNotFoundException{
+    public static void carregar_arquivo( ArrayList<Eleitor> ListaEleitor ) throws FileNotFoundException, IOException{
    
     Eleitor e;
     String nome, titulo, data_nascimento, secao, zona;
     int cond2 = 0;
     File arquivo = new File ("ListaEleitor.txt");
+    
+    boolean existe = arquivo.exists();
+            if(!existe){
+                arquivo.createNewFile();
+            }  
    
         //Carrega todo conteúdo do arquivo
     Scanner scanner = new Scanner(new FileReader("ListaEleitor.txt")).useDelimiter("\\||\\n");
--- TP2-UrnaEletronica/src/tp2/Validar.java
+++
@@ -1,6 +0,0 @@
-package tp2;
-
-public interface Validar {
-    
-    public abstract boolean Validar(String voto);
-}
